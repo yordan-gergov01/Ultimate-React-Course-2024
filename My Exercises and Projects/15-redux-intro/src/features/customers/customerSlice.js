f2f4c186eb37eddde4280932002088c8cfaa2cd5
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-22 19:27:51 +0300

    Add files via upload

---
+++ My Exercises and Projects/15-redux-intro/src/features/customers/customerSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  fullName: "",
+  nationalId: "",
+  createdAt: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateCustomer(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateCustomer } = customerSlice.actions;
+
+export default customerSlice.reducer;
